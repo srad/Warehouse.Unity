ce4fedac60b74287ca1ef505f1f419a45b6aff92
srad <ssedighi@posteo.de>
2020-07-09 15:31:43 +0200

    first commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+  public CharacterController controller;
+  public float Speed = 12f;
+  public float RotateSpeed = 3.0F;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+        
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    /*
+    float x = Input.GetAxis("Horizontal");
+    float z = Input.GetAxis("Vertical");
+
+    var move = transform.right * x + transform.forward * z;
+    controller.Move(move * Speed * Time.deltaTime);
+    */
+    transform.Rotate(0, Input.GetAxis("Horizontal") * RotateSpeed, 0);
+    var forward = transform.TransformDirection(Vector3.forward);
+    float curSpeed = Speed * Input.GetAxis("Vertical");
+    controller.SimpleMove(forward * curSpeed);
+  }
+}
---
+++ Assets/Scripts/Events/Screenshot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Screenshot : MonoBehaviour
+{
+  public Canvas HudCanvas;
+
+  public void SaveScreenshot()
+  {
+    HudCanvas.enabled = false;
+    var path = Application.dataPath;
+    ScreenCapture.CaptureScreenshot(path + "/screen_" + DateTime.Now.ToShortDateString() + ".png");
+    HudCanvas.enabled = true;
+    Application.OpenURL("file://" + path);
+  }
+}
