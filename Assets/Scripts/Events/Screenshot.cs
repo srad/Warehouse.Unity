15a0be08fbb5617ca244ad99fd3197a7dcf3ff32
srad <ssedighi@posteo.de>
2020-07-10 11:05:06 +0200

    Camera Render hide UI

--- Assets/Scripts/Events/Screenshot.cs
+++ Assets/Scripts/Events/Screenshot.cs
@@ -2,18 +2,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UIElements;
+using System.IO;
 
 public class Screenshot : MonoBehaviour
 {
-  public Canvas HudCanvas;
+    public GameObject panel;
+    private string _screenshotPath;
 
-  public void SaveScreenshot()
-  {
-    HudCanvas.enabled = false;
-    var path = Application.dataPath;
-    ScreenCapture.CaptureScreenshot(path + "/screen_" + DateTime.Now.ToShortDateString() + ".png");
-    HudCanvas.enabled = true;
-    Application.OpenURL("file://" + path);
-  }
+    public void SaveScreenshot()
+    {
+        _screenshotPath = Path.Combine(Application.dataPath, "Screenshots");
+        if (!Directory.Exists(_screenshotPath))
+        {
+            Directory.CreateDirectory(_screenshotPath);
+        }
+
+        panel.gameObject.SetActive(false);
+
+        StartCoroutine(WaitForEndOfFrameCoroutine(1000));
+    }
+
+    private IEnumerator WaitForEndOfFrameCoroutine(float time)
+    {
+        yield return (new WaitForEndOfFrame());
+        ScreenCapture.CaptureScreenshot(_screenshotPath + "/screen_" + Directory.GetFiles(_screenshotPath, "*.png").Length.ToString("D4") + ".png");
+        panel.gameObject.SetActive(true);
+        Application.OpenURL("file://" + _screenshotPath);
+    }
 }
