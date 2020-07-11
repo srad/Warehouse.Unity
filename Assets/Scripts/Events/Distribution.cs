12fd16e81ec1bd24eed70068c8a3d106cdd377e0
srad <ssedighi@posteo.de>
2020-07-11 23:21:31 +0200

    Better random generation

---
+++ Assets/Scripts/Events/Distribution.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Events
+{
+    public class Element<T>
+    {
+        public double Probability { get; set; }
+        public T Item { get; set; }
+    }
+
+    /// <summary>
+    /// Details please see: https://stackoverflow.com/questions/46735106/pick-random-element-from-list-with-probability
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Distribution<T> : List<Element<T>>
+    {
+        private List<Element<T>> _sumProbabilities;
+
+        public void Init()
+        {
+            _sumProbabilities = new List<Element<T>>(Count);
+
+            var sum = 0.0;
+
+            foreach (var item in this.Take(Count - 1))
+            {
+                sum += item.Probability;
+                _sumProbabilities.Add(new Element<T> {Probability = sum, Item = item.Item});
+            }
+
+            _sumProbabilities.Add(new Element<T> {Probability = 1.0, Item = this.Last().Item});
+        }
+
+        public T Sample()
+        {
+            var probability = Random.Range(0f, 1f);
+            return _sumProbabilities.SkipWhile(i => i.Probability < probability).First().Item;
+        }
+    }
+}
--- Assets/Scripts/Events/GenerateWarehouse.cs
+++ Assets/Scripts/Events/GenerateWarehouse.cs
@@ -1,17 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
+using Events;
 using UnityEditor;
 using UnityEngine;
 
 public class GenerateWarehouse : MonoBehaviour
 {
-    private class Prob<T>
-    {
-        public double Probability { get; set; }
-        public T Item { get; set; }
-    }
-
     [Header("Clone Objects")] public GameObject pallet;
     public GameObject shelf;
 
@@ -25,22 +19,20 @@ public class GenerateWarehouse : MonoBehaviour
 
     [Header("Distance between each object")]
     public float xOffset = 1.8f;
-
-    public float zOffset = -1.3f;
-    public float yOffset = 0f;
+    public float zOffset = -1.6f;
 
     public float zShelfOffset = 11f;
     public float xShelfOffset = 1.85f;
 
     [Header("Z Group")] public int zCount = 18;
-    public int zGroupSize = 7;
-    public float zGroupDistance = -0.7f;
+    public int zGroupSize = 6;
+    public float zGroupDistance = -0.8f;
 
     [Header("X Group")] public int xCount = 6;
     public int xGroupSize = 2;
     public float xGroupDistance = 5f;
 
-    [Header("Damage Probabilities")] public float pDamage = 0.2f;
+    [Header("Damage Probabilities")] public float pDamage = 0.25f;
     public float pBrickMissing = 0.1f;
 
     public float pTopPlankMissing = 0.01f;
@@ -49,43 +41,51 @@ public class GenerateWarehouse : MonoBehaviour
 
     public float pRotationBrick = 0.1f;
     public float pRotationPallet = 0.1f;
-    
+
     [Header("Damage Ranges")] public float palletRotation = 2f;
     public float zRange = 0.05f;
     public float xRange = 0.1f;
 
-    private List<Prob<Material>> _materialProbability;
-    private List<Prob<Material>> _sumProbabilities;
+    private Distribution<Material> _matDist;
+    private Distribution<int> _heightDist;
 
-    /// <summary>
-    /// See https://stackoverflow.com/questions/46735106/pick-random-element-from-list-with-probability
-    /// </summary>
     private void InitMaterialProbabilities()
     {
-        _materialProbability = new List<Prob<Material>>()
+        _matDist = new Distribution<Material>()
         {
-            new Prob<Material> {Item = material1, Probability = pMaterial1},
-            new Prob<Material> {Item = material2, Probability = pMaterial2},
-            new Prob<Material> {Item = material3, Probability = pMaterial3}
+            new Element<Material> {Item = material1, Probability = pMaterial1},
+            new Element<Material> {Item = material2, Probability = pMaterial2},
+            new Element<Material> {Item = material3, Probability = pMaterial3}
         };
+        _matDist.Init();
 
-        _sumProbabilities = new List<Prob<Material>>(_materialProbability.Count);
-
-        var sum = 0.0;
-
-        foreach (var item in _materialProbability.Take(_materialProbability.Count - 1))
+        _heightDist = new Distribution<int>()
         {
-            sum += item.Probability;
-            _sumProbabilities.Add(new Prob<Material> {Probability = sum, Item = item.Item});
-        }
-
-        _sumProbabilities.Add(new Prob<Material> {Probability = 1.0, Item = _materialProbability.Last().Item});
+            new Element<int> {Item = 3, Probability = 0.7f},
+            new Element<int> {Item = 2, Probability = 0.3f},
+            new Element<int> {Item = 1, Probability = 0.2f}
+        };
+        _heightDist.Init();
     }
 
-    private Material PickRandomMaterial()
+    private int BoxToLayer(string name)
     {
-        var probability = Random.Range(0f, 1f);
-        return _sumProbabilities.SkipWhile(i => i.Probability < probability).First().Item;
+        if (name.StartsWith("Box.Layer1"))
+        {
+            return 1;
+        }
+
+        if (name.StartsWith("Box.Layer2"))
+        {
+            return 2;
+        }
+
+        if (name.StartsWith("Box.Layer3"))
+        {
+            return 3;
+        }
+
+        return 3;
     }
 
     public void Start()
@@ -105,50 +105,67 @@ public class GenerateWarehouse : MonoBehaviour
             // -z Direction
             for (var z = 0; z < zCount; z++)
             {
-                var newInstance = Instantiate(pallet);
-                newInstance.transform.position = new Vector3(xPosition, start.y, zPosition);
-                newInstance.transform.Rotate(Vector3.up, Random.Range(-palletRotation, palletRotation));
-                newInstance.transform.Translate(Random.Range(-xRange, xRange), 0f, 0f);
-                newInstance.transform.Translate(0f, 0f, Random.Range(-zRange, zRange));
+                var newPallet = Instantiate(pallet);
+                newPallet.transform.position = new Vector3(xPosition, start.y, zPosition);
+                newPallet.transform.Rotate(Vector3.up, Random.Range(-palletRotation, palletRotation));
+                newPallet.transform.Translate(Random.Range(-xRange, xRange), 0f, 0f);
+                newPallet.transform.Translate(0f, 0f, Random.Range(-zRange, zRange));
 
-                var mat = PickRandomMaterial();
+                var mat = _matDist.Sample();
+                var height = _heightDist.Sample();
 
-                for (var j = 0; j < newInstance.transform.childCount; j++)
+                for (var j = 0; j < newPallet.transform.childCount; j++)
                 {
-                    // Material
-                    var child = newInstance.transform.GetChild(j);
+                    // Pallet
+                    var child = newPallet.transform.GetChild(j);
                     if (child.name.StartsWith("Pallet"))
                     {
                         child.GetComponent<Renderer>().material = mat;
+
+                        // Damage
+                        if (Random.Range(0f, 1f) < pDamage)
+                        {
+                            if (child.name.StartsWith("Pallet.Plank.Top"))
+                            {
+                                child.gameObject.SetActive(Random.Range(0f, 1.0f) > pTopPlankMissing);
+                            }
+
+                            if (child.name.StartsWith("Pallet.Plank.Middle"))
+                            {
+                                child.gameObject.SetActive(Random.Range(0f, 1.0f) > pMiddlePlankMissing);
+                            }
+
+                            if (child.name.StartsWith("Pallet.Plank.Bottom"))
+                            {
+                                child.gameObject.SetActive(Random.Range(0f, 1.0f) > pBottomPlankMissing);
+                            }
+
+                            if (child.name.StartsWith("Pallet.Brick"))
+                            {
+                                child.gameObject.SetActive(Random.Range(0f, 1.0f) < pBrickMissing);
+                            }
+                        }
                     }
 
-                    // Damage
-                    if (Random.Range(0f, 1f) < pDamage)
+                    // Box
+                    if (child.name.StartsWith("Box.Layer"))
                     {
-                        if (child.name.StartsWith("Pallet.Plank.Top"))
+                        var layerHeight = BoxToLayer(child.name);
+                        if (layerHeight > height)
                         {
-                            child.gameObject.SetActive(Random.Range(0f, 1.0f) > pTopPlankMissing);
+                            child.gameObject.SetActive(false);
                         }
 
-                        if (child.name.StartsWith("Pallet.Plank.Middle"))
+                        // Randomly toggle objects on top most layer
+                        if (layerHeight == height)
                         {
-                            child.gameObject.SetActive(Random.Range(0f, 1.0f) > pMiddlePlankMissing);
-                        }
-
-                        if (child.name.StartsWith("Pallet.Plank.Bottom"))
-                        {
-                            child.gameObject.SetActive(Random.Range(0f, 1.0f) > pBottomPlankMissing);
-                        }
-
-                        if (child.name.StartsWith("Pallet.Brick"))
-                        {
-                            child.gameObject.SetActive(Random.Range(0f, 1.0f) < pBrickMissing);
+                            child.gameObject.SetActive(Random.Range(0f, 1f) < 0.6f);
                         }
                     }
                 }
 
                 zPosition += zOffset;
-                if (z % zGroupSize == 0)
+                if (z > 0 && z % zGroupSize == 0)
                 {
                     var newShelf = Instantiate(shelf);
                     newShelf.transform.position = new Vector3(xPositionShelf, shelf.transform.position.y, zPositionShelf);
@@ -167,6 +184,7 @@ public class GenerateWarehouse : MonoBehaviour
             }
         }
 
+        // Remove the template GameObjects
         shelf.SetActive(false);
         pallet.SetActive(false);
     }
