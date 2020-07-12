8f2a360b047af7a82f6e97010b6d204050f3498c
srad <ssedighi@posteo.de>
2020-07-12 19:10:57 +0200

    Label rendering and template for automated data generation

--- Assets/Scripts/Events/GenerateWarehouse.cs
+++ Assets/Scripts/Events/GenerateWarehouse.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Events;
 using UnityEditor;
 using UnityEngine;
@@ -35,43 +36,57 @@ public class GenerateWarehouse : MonoBehaviour
 
     [Header("General Probabilities")] public float pLoaded = 0.8f;
     public float pDamage = 0.25f;
+    public float pPalletMissing = 0.05f;
 
     [Header("Damage Probabilities")] public float pBrickMissing = 0.1f;
-
     public float pTopPlankMissing = 0.01f;
     public float pMiddlePlankMissing = 0.01f;
     public float pBottomPlankMissing = 0.01f;
+    public float pBrickDamage = 0.2f;
 
     public float pRotationBrick = 0.15f;
-    public float pRotationPallet = 0.1f;
 
     [Header("Damage Ranges")] public float palletRotation = 2f;
     public float zRange = 0.05f;
     public float xRange = 0.1f;
 
-    private Distribution<Material> _matDist;
-    private Distribution<int> _heightDist;
+    private Distribution<Material> _distMaterial;
+    private Distribution<int> _distStackHeight;
+    private Distribution<string> _distBrickDamage;
 
     private void InitMaterialProbabilities()
     {
-        _matDist = new Distribution<Material>()
+        _distMaterial = new Distribution<Material>
         {
             new Element<Material> {Item = material1, Probability = pMaterial1},
             new Element<Material> {Item = material2, Probability = pMaterial2},
             new Element<Material> {Item = material3, Probability = pMaterial3}
         };
-        _matDist.Init();
+        _distMaterial.Init();
 
-        _heightDist = new Distribution<int>()
+        _distStackHeight = new Distribution<int>
         {
             new Element<int> {Item = 4, Probability = 0.5f},
             new Element<int> {Item = 3, Probability = 0.3f},
             new Element<int> {Item = 2, Probability = 0.1f},
             new Element<int> {Item = 1, Probability = 0.1f}
         };
-        _heightDist.Init();
+        _distStackHeight.Init();
+
+        _distBrickDamage = new Distribution<string>
+        {
+            new Element<string> {Item = "Pallet.Brick.Corner", Probability = 0.1f},
+            new Element<string> {Item = "Pallet.Brick.Side", Probability = 0.2f},
+            new Element<string> {Item = "Pallet.Brick.Front", Probability = 0.6f}
+        };
+        _distBrickDamage.Init();
     }
 
+    /// <summary>
+    /// Don't use regex, slow AF.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
     private int BoxToLayer(string name)
     {
         if (name.StartsWith("Box.Layer1"))
@@ -89,7 +104,22 @@ public class GenerateWarehouse : MonoBehaviour
             return 3;
         }
 
-        return 3;
+        if (name.StartsWith("Box.Layer4"))
+        {
+            return 4;
+        }
+
+        return 4;
+    }
+
+    private GameObject CreatePallet(Vector3 pos)
+    {
+        var newPallet = Instantiate(pallet);
+        newPallet.transform.position = pos;
+        newPallet.transform.Rotate(Vector3.up, Random.Range(-palletRotation, palletRotation));
+        newPallet.transform.Translate(Random.Range(-xRange, xRange), 0f, 0f);
+        newPallet.transform.Translate(0f, 0f, Random.Range(-zRange, zRange));
+        return newPallet;
     }
 
     public void Start()
@@ -109,77 +139,88 @@ public class GenerateWarehouse : MonoBehaviour
             // -z Direction
             for (var z = 0; z < zCount; z++)
             {
-                var loadPallet = Random.Range(0f, 1f) < pLoaded;
-                var newPallet = Instantiate(pallet);
-                newPallet.transform.position = new Vector3(xPosition, start.y, zPosition);
-                newPallet.transform.Rotate(Vector3.up, Random.Range(-palletRotation, palletRotation));
-                newPallet.transform.Translate(Random.Range(-xRange, xRange), 0f, 0f);
-                newPallet.transform.Translate(0f, 0f, Random.Range(-zRange, zRange));
-
-                var mat = _matDist.Sample();
-                var height = _heightDist.Sample();
-
-                for (var j = 0; j < newPallet.transform.childCount; j++)
+                if (pPalletMissing < Random.Range(0f, 1f))
                 {
-                    // Pallet
-                    var child = newPallet.transform.GetChild(j);
-                    if (child.name.StartsWith("Pallet"))
+                    var loadPallet = Random.Range(0f, 1f) < pLoaded;
+                    var newPallet = CreatePallet(new Vector3(xPosition, start.y, zPosition));
+                    var mat = _distMaterial.Sample();
+                    var height = _distStackHeight.Sample();
+
+                    // Traverse children, planks, bricks, etc.
+                    for (var j = 0; j < newPallet.transform.childCount; j++)
                     {
-                        child.GetComponent<Renderer>().material = mat;
-
-                        // Damage
-                        if (Random.Range(0f, 1f) < pDamage)
+                        // Pallet
+                        var child = newPallet.transform.GetChild(j);
+                        if (child.name.StartsWith("Pallet."))
                         {
-                            if (child.name.StartsWith("Pallet.Plank.Top"))
-                            {
-                                child.gameObject.SetActive(Random.Range(0f, 1.0f) > pTopPlankMissing);
-                            }
+                            // Assign material to all parts
+                            var m = Instantiate(mat);
+                            // Vary surface grain/texture
+                            m.mainTextureOffset = new Vector2(Random.Range(-100f, 100f), Random.Range(-50f, 50f));
+                            child.GetComponent<Renderer>().material = m;
 
-                            if (child.name.StartsWith("Pallet.Plank.Middle"))
+                            // Damage
+                            if (Random.Range(0f, 1f) > pDamage)
                             {
-                                child.gameObject.SetActive(Random.Range(0f, 1.0f) > pMiddlePlankMissing);
-                            }
-
-                            if (child.name.StartsWith("Pallet.Plank.Bottom"))
-                            {
-                                child.gameObject.SetActive(Random.Range(0f, 1.0f) > pBottomPlankMissing);
-                            }
-
-                            if (child.name.StartsWith("Pallet.Brick"))
-                            {
-                                child.gameObject.SetActive(Random.Range(0f, 1.0f) < pBrickMissing);
-                            }
-
-                            if (child.name.StartsWith("Pallet.Brick"))
-                            {
-                                if (Random.Range(0f, 1f) < pRotationBrick)
+                                var appliedDamage = false;
+                                if (child.name.StartsWith("Pallet.Plank.Top"))
                                 {
-                                    child.transform.RotateAround(child.GetComponent<Renderer>().bounds.center, Vector3.up, Random.Range(-10f, 10f));
+                                    child.gameObject.SetActive(Random.Range(0f, 1.0f) > pTopPlankMissing);
+                                    appliedDamage = true;
+                                }
+
+                                if (child.name.StartsWith("Pallet.Plank.Middle"))
+                                {
+                                    child.gameObject.SetActive(Random.Range(0f, 1.0f) > pMiddlePlankMissing);
+                                    appliedDamage = true;
+                                }
+
+                                if (child.name.StartsWith("Pallet.Plank.Bottom"))
+                                {
+                                    child.gameObject.SetActive(Random.Range(0f, 1.0f) > pBottomPlankMissing);
+                                    appliedDamage = true;
+                                }
+
+                                if (child.name.StartsWith("Pallet.Brick"))
+                                {
+                                    child.gameObject.SetActive(Random.Range(0f, 1.0f) > pBrickMissing);
+                                    appliedDamage = true;
+                                }
+
+                                if (child.name.StartsWith("Pallet.Brick") && Random.Range(0f, 1.0f) > pBrickDamage)
+                                {
+                                    var brick = _distBrickDamage.Sample();
+                                    if (child.name.StartsWith(brick))
+                                    {
+                                        child.transform.RotateAround(child.GetComponent<Renderer>().bounds.center, Vector3.up, Random.Range(-20f, 20f));
+                                        appliedDamage = true;
+                                    }
                                 }
                             }
                         }
-                    }
 
-                    // Box
-                    if (child.name.StartsWith("Box.Layer"))
-                    {
-                        if (loadPallet)
+                        // Box
+                        if (child.name.StartsWith("Box.Layer"))
                         {
-                            var layerHeight = BoxToLayer(child.name);
-                            if (layerHeight > height)
+                            child.transform.RotateAround(child.GetChild(0).GetComponent<Renderer>().bounds.center, Vector3.up, Random.Range(-5f, 5f));
+                            if (loadPallet)
+                            {
+                                var layerHeight = BoxToLayer(child.name);
+                                if (layerHeight > height)
+                                {
+                                    child.gameObject.SetActive(false);
+                                }
+
+                                // Randomly toggle objects on top most layer
+                                if (layerHeight == height)
+                                {
+                                    child.gameObject.SetActive(Random.Range(0f, 1f) < 0.5f);
+                                }
+                            }
+                            else
                             {
                                 child.gameObject.SetActive(false);
                             }
-
-                            // Randomly toggle objects on top most layer
-                            if (layerHeight >= height)
-                            {
-                                child.gameObject.SetActive(Random.Range(0f, 1f) < 0.7f);
-                            }
-                        }
-                        else
-                        {
-                            child.gameObject.SetActive(false);
                         }
                     }
                 }
--- Assets/Scripts/Events/Screenshot.cs
+++ Assets/Scripts/Events/Screenshot.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -16,14 +17,24 @@ public class Screenshot : MonoBehaviour
         public Material Material;
     }
 
-    public GameObject panel;
+    [Header("Materials")] public GameObject panel;
     public Material labelMaterial;
-    public Camera renderCamera;
+    public Color brickCornerColor;
+    public Color brickSideColor;
+    public Color brickCenterColor;
+    public Color brickFrontColor;
+    public Color plankTopColor;
+    public Color plankBottomColor;
+    public Color plankMiddleColor;
+
+    [Header("Setup")] public Camera renderCamera;
     public string HideTag;
     public string TargetTag;
 
+    #region Private
+
     private string _screenshotPath;
-    private int ScreenshotCount => Directory.GetFiles(_screenshotPath, "*_0_.png").Length;
+    private string ScreenshotPrefix => Guid.NewGuid().ToString(); //Directory.GetFiles(_screenshotPath, "*_0_.png").Length;
     private IEnumerable<GameObject> TargetObjects => GameObject.FindGameObjectsWithTag(TargetTag);
     private IEnumerable<GameObject> HideObjects => GameObject.FindGameObjectsWithTag(HideTag);
 
@@ -39,7 +50,7 @@ public class Screenshot : MonoBehaviour
     /// </summary>
     private void Start()
     {
-        _screenshotPath = Path.Combine(Application.dataPath, "Screenshots");
+        _screenshotPath = Path.Combine(Application.dataPath, "..", "Screenshots");
         if (!Directory.Exists(_screenshotPath))
         {
             Directory.CreateDirectory(_screenshotPath);
@@ -51,46 +62,110 @@ public class Screenshot : MonoBehaviour
     /// 1. Main camera view rendered without UI
     /// 2. Same rendering as 1. but all visible target object is highlighted
     /// </summary>
-    private async void WriteScreenshot()
+    private IEnumerator WriteScreenshot()
     {
         // Retains the original game objects with the original material
-        var objects = new List<GameObjectInfo>();
-        var count = ScreenshotCount;
+        var originalState = new List<GameObjectInfo>();
+        var hiddenObjects = new List<GameObject>();
+        var filename = ScreenshotPrefix;
 
         // 1. First rendering without UI
         panel.gameObject.SetActive(false);
-        StartCoroutine(WaitForEndOfFrameCoroutine("", count, "_0_"));
-        await Task.Delay(50);
+        yield return StartCoroutine(Capture(filename, "_0_"));
+        yield return new WaitForEndOfFrame();
 
+        // 2. Label entire pallet with one material
         foreach (var target in VisibleTargetObjects)
         {
-            var objRenderer = target.GetComponent<Renderer>();
-            objects.Add(new GameObjectInfo {Material = objRenderer.material, Renderer = objRenderer});
-            await Task.Delay(50);
-            objRenderer.material = null;
-            objRenderer.materials = new[] {labelMaterial};
-            objRenderer.shadowCastingMode = ShadowCastingMode.Off;
-            objRenderer.receiveShadows = false;
+            for (var i = 0; i < target.transform.childCount; i++)
+            {
+                if (target.transform.GetChild(i).name.StartsWith("Pallet."))
+                {
+                    var child = target.transform.GetChild(i).GetComponent<Renderer>();
+                    originalState.Add(new GameObjectInfo {Material = new Material(child.material), Renderer = child});
+                    yield return new WaitForEndOfFrame();
+                    child.material = null;
+                    child.materials = new[] {labelMaterial};
+                    child.shadowCastingMode = ShadowCastingMode.Off;
+                    child.receiveShadows = false;
+                }
+                // Hide non essential parts
+                else if (target.transform.GetChild(i).name.Equals("Pallet"))
+                {
+                    hiddenObjects.Add(target.transform.GetChild(i).gameObject);
+                    target.transform.GetChild(i).gameObject.SetActive(false);
+                }
+            }
+
+            yield return new WaitForEndOfFrame();
         }
 
-        StartCoroutine(WaitForEndOfFrameCoroutine("", count, "_1_"));
-        await Task.Delay(50);
+        yield return StartCoroutine(Capture(filename, "_1_"));
+        yield return new WaitForEndOfFrame();
 
-        // Restore original materials
-        foreach (var obj in objects)
+        // 3. Label each part with different color
+        foreach (var target in VisibleTargetObjects)
         {
-            obj.Renderer.materials = new[] {obj.Material};
+            for (var i = 0; i < target.transform.childCount; i++)
+            {
+                var name = target.transform.GetChild(i).name;
+                if (name.StartsWith("Pallet."))
+                {
+                    var child = target.transform.GetChild(i).GetComponent<Renderer>();
+                    child.material = null;
+                    var mat = Instantiate(labelMaterial);
+                    mat.color = GetPartMaterial(name);
+                    child.materials = new[] {mat};
+                    child.shadowCastingMode = ShadowCastingMode.Off;
+                    child.receiveShadows = false;
+                    yield return new WaitForEndOfFrame();
+                }
+            }
+        }
+
+        yield return StartCoroutine(Capture(filename, "_2_"));
+        yield return new WaitForEndOfFrame();
+
+        // 4. Restore original materials and show hidden stuff
+        foreach (var obj in originalState)
+        {
+            obj.Renderer.materials = new[] {new Material(obj.Material)};
             obj.Renderer.shadowCastingMode = ShadowCastingMode.On;
             obj.Renderer.receiveShadows = true;
+        }
+
+        foreach (var obj in hiddenObjects)
+        {
+            obj.SetActive(true);
         }
 
         panel.gameObject.SetActive(true);
     }
 
-    private IEnumerator WaitForEndOfFrameCoroutine(string filename, int index, string postfix)
+    private Color GetPartMaterial(string name)
+    {
+        if (name.StartsWith("Pallet.Brick.Corner"))
+            return brickCornerColor;
+        if (name.StartsWith("Pallet.Brick.Side"))
+            return brickSideColor;
+        if (name.StartsWith("Pallet.Brick.Center"))
+            return brickCenterColor;
+        if (name.StartsWith("Pallet.Brick.Front"))
+            return brickFrontColor;
+        if (name.StartsWith("Pallet.Plank.Top"))
+            return plankTopColor;
+        if (name.StartsWith("Pallet.Plank.Bottom"))
+            return plankBottomColor;
+        if (name.StartsWith("Pallet.Plank.Middle"))
+            return plankMiddleColor;
+
+        return new Color(0.1f, 0.1f, 0.1f, 1);
+    }
+
+    private IEnumerator Capture(string filename, string postfix)
     {
         yield return new WaitForEndOfFrame();
-        ScreenCapture.CaptureScreenshot(_screenshotPath + "/" + filename + index.ToString("D4") + postfix + ".png");
+        ScreenCapture.CaptureScreenshot(_screenshotPath + "/" + filename + postfix + ".png");
         //Application.OpenURL("file://" + _screenshotPath);
     }
 
@@ -98,7 +173,7 @@ public class Screenshot : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            WriteScreenshot();
+            StartCoroutine(WriteScreenshot());
         }
     }
 
@@ -107,4 +182,6 @@ public class Screenshot : MonoBehaviour
         var planes = GeometryUtility.CalculateFrustumPlanes(renderCamera);
         return GeometryUtility.TestPlanesAABB(planes, r.bounds);
     }
+
+    #endregion
 }
